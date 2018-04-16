2f9095ddc4fc56d4a78ad6606425af6eaa2c2355
Dahlia75 <d.aljabari75@gmail.com>
2018-04-16 06:22:32 +0000

    Read the data

--- https_example.js
+++ https_example.js
@@ -12,10 +12,19 @@ var options = {
 //   console.log('In response handler callback!');
 // }
 
-// called by https when the request is made.
+// // called by https when the request is made.
+// var callback = function(response) {
+//   console.log('In response handler callback!');
+//   console.log('Response: ', response);
+// }
+
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
